47896b0a88c3abd4d895d1cb72cbe4ec55e9696c
Minh An <106749793+MinhAn15@users.noreply.github.com>
2024-04-08 09:57:50 +0700

    Update backend.py

--- backend.py
+++ backend.py
@@ -126,7 +126,7 @@ def index_export(user_input):
     "Using only the context information to "
     "answer the question: {query_str}\n"
     "3 Rules to follow:\n"
-    "1.If the context isn't helpful, you said that information is not available and suggest by your own knowledge.\n"
+    "1.If the context information isn't helpful, use vietnamese said that information is not available and suggest by your own knowledge.\n"
     "2.Always Use vietnamese to answer.\n"
     "3.Use bullet point and human's natural tone.")
         
